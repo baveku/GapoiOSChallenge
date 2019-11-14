1ebd315194b7ceb3c67bc089bfdb6afa816f351e
vuzbach <baveku@gmail.com>
2019-11-14 17:17:58 +0700

    Initial Commit

---
+++ GapoiOSChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GapoiOSChallenge
+//
+//  Created by Bách veku on 11/14/19.
+//  Copyright © 2019 Bách veku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GapoiOSChallengeTests/GapoiOSChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  GapoiOSChallengeTests.swift
+//  GapoiOSChallengeTests
+//
+//  Created by Bách veku on 11/14/19.
+//  Copyright © 2019 Bách veku. All rights reserved.
+//
+
+import XCTest
+@testable import GapoiOSChallenge
+
+class GapoiOSChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
